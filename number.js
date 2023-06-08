2b8c58706df4d9e5f7ee996f81670cc49522b842
ÖFFK <110122698+FatihKarakass@users.noreply.github.com>
2023-06-08 23:51:28 +0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,55 @@
+let price = 100
+let tax =0.18
+let priceTax= price * tax
+let total =price + priceTax
+console.log(total)
+
+console.log(
+    "Fiyat; ", price 
+    ," KDV Orani; ", tax
+    ," KDV Tutari; ", priceTax
+    ," Fiyat ; " , total
+)
+
+let stringNumber ="11"
+console.log(stringNumber)
+let newNumber = Number(stringNumber)
+
+let counter =320
+counter =counter +1
+counter ++;
+counter +=1;
+
+counter --;
+counter -=1;
+counter= counter - 1;
+
+console.log(counter)
+
+let carpbol =20;
+carpbol *= 5 ;
+console.log(carpbol)
+
+carpbol /= 2 ;
+console.log(carpbol)
+
+console.log( (2+5+4+6)*5 )
+
+console.log(17 % 2) //tek ise 1 çift ise 0
+
+console.log("Kuli kalan urun :" , 20 % 8 )
+
+console.log(2 **6) //üs alma
+console.log(2*2*2*2*2*2)
+
+console.log("Assagi yuvarlama:" , Math.floor(1.9))
+
+console.log("Yukari yuvarlam :  " ,Math.ceil(1.9))
+
+console.log("Yakina yuvarlma: " , Math.round(1.5))
+
+
+
+
+
+
